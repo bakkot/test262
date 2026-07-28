06f8ad8dc46a1f7f2f903818ab50861e1dc453b0
Kevin Gibbons <bakkot@gmail.com>
2026-07-28 16:15:45 -0700

    update test for Array.prototype[%Symbol.unscopables%]

--- test/built-ins/Array/prototype/Symbol.unscopables/value.js
+++ test/built-ins/Array/prototype/Symbol.unscopables/value.js
@@ -1,26 +1,9 @@
 // Copyright (C) 2015 Mike Pennisi. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
-esid: sec-array.prototype-@@unscopables
+esid: sec-array.prototype-%symbol.unscopables%
 description: >
     Initial value of `Symbol.unscopables` property
-info: |
-    22.1.3.32 Array.prototype [ @@unscopables ]
-
-    1. Let unscopableList be ObjectCreate(null).
-    2. Perform CreateDataProperty(unscopableList, "copyWithin", true).
-    3. Perform CreateDataProperty(unscopableList, "entries", true).
-    4. Perform CreateDataProperty(unscopableList, "fill", true).
-    5. Perform CreateDataProperty(unscopableList, "find", true).
-    6. Perform CreateDataProperty(unscopableList, "findIndex", true).
-    7. Perform CreateDataProperty(unscopableList, "flat", true).
-    8. Perform CreateDataProperty(unscopableList, "flatMap", true).
-    9. Perform CreateDataProperty(unscopableList, "includes", true).
-    10. Perform CreateDataProperty(unscopableList, "keys", true).
-    11. Perform CreateDataProperty(unscopableList, "values", true).
-    12. Assert: Each of the above calls returns true.
-    13. Return unscopableList.
-
 includes: [propertyHelper.js]
 features: [Symbol.unscopables]
 ---*/
@@ -28,6 +11,13 @@ features: [Symbol.unscopables]
 var unscopables = Array.prototype[Symbol.unscopables];
 
 assert.sameValue(Object.getPrototypeOf(unscopables), null);
+
+assert.sameValue(unscopables.at, true, '`at` property value');
+verifyProperty(unscopables, "at", {
+  writable: true,
+  enumerable: true,
+  configurable: true
+});
 
 assert.sameValue(unscopables.copyWithin, true, '`copyWithin` property value');
 verifyProperty(unscopables, "copyWithin", {
@@ -64,6 +54,20 @@ verifyProperty(unscopables, "findIndex", {
   configurable: true
 });
 
+assert.sameValue(unscopables.findLast, true, '`findLast` property value');
+verifyProperty(unscopables, "findLast", {
+  writable: true,
+  enumerable: true,
+  configurable: true
+});
+
+assert.sameValue(unscopables.findLastIndex, true, '`findLastIndex` property value');
+verifyProperty(unscopables, "findLastIndex", {
+  writable: true,
+  enumerable: true,
+  configurable: true
+});
+
 assert.sameValue(unscopables.flat, true, '`flat` property value');
 verifyProperty(unscopables, "flat", {
   writable: true,
@@ -87,6 +91,27 @@ verifyProperty(unscopables, "includes", {
 
 assert.sameValue(unscopables.keys, true, '`keys` property value');
 verifyProperty(unscopables, "keys", {
+  writable: true,
+  enumerable: true,
+  configurable: true
+});
+
+assert.sameValue(unscopables.toReversed, true, '`toReversed` property value');
+verifyProperty(unscopables, "toReversed", {
+  writable: true,
+  enumerable: true,
+  configurable: true
+});
+
+assert.sameValue(unscopables.toSorted, true, '`toSorted` property value');
+verifyProperty(unscopables, "toSorted", {
+  writable: true,
+  enumerable: true,
+  configurable: true
+});
+
+assert.sameValue(unscopables.toSpliced, true, '`toSpliced` property value');
+verifyProperty(unscopables, "toSpliced", {
   writable: true,
   enumerable: true,
   configurable: true
